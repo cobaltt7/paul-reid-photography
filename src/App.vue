6f53b3bda46d884b59383fc3490737fd1830aff0
RedGuy12 <paul@reid-family.org>
2022-01-11 11:48:07 -0600

    feat(ui,pages): Add a footer
    Closes #15

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,9 @@
 		<header
 			class="border-b border-red-800 flex items-end justify-between m-auto p-6 pb-0 w-11/12"
 		>
-			<h1 class="pb-4 text-5xl text-yellow-900">Paul Reid Photography</h1>
+			<router-link to="/">
+				<h1 class="pb-4 text-5xl text-yellow-900">Paul Reid Photography</h1>
+			</router-link>
 			<!--
 			  <nav>
 			  <router-link to="/">Featured</router-link>
@@ -17,6 +19,24 @@
 		<main>
 			<router-view />
 		</main>
+		<footer
+			class="border-red-800 border-t m-auto p-6 pt-1 text-sm w-11/12"
+		>
+			<a href="https://github.com/RedGuy12/paul-reid-photography">Source code</a> licensed
+			under
+			<a href="https://github.com/RedGuy12/paul-reid-photography/blob/master/LICENSE"
+				>the MIT License</a
+			>.
+			<a
+				href="https://github.com/RedGuy12/paul-reid-photography/tree/master/public/img/photos"
+				>All photographs</a
+			>
+			copyright 2022 Paul Reid,
+			<a
+				href="https://github.com/RedGuy12/paul-reid-photography/blob/master/public/img/photos/LICENSE"
+				>All Rights Reserved</a
+			>.
+		</footer>
 	</body>
 </template>
 
