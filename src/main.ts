4fce336d3a9d28678a8b38d121695448b4a5f785
RedGuy12 <paul@reid-family.org>
2022-07-28 18:36:32 -0500

    fix(api): Remove service worker
    Signed-off-by: RedGuy12 <paul@reid-family.org>

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
-/** @file Render Page. */
-
 import { createHead } from "@vueuse/head";
 import { createApp } from "vue";
 
 import App from "./App.vue";
-// eslint-disable-next-line import/no-unassigned-import -- This is not a module.
-import "./lib/registerServiceWorker";
 import router from "./lib/router";
 
 const app = createApp(App);
